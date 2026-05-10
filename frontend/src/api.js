470586f8caedb85115e4ebfbfd22ebf87826ae20
Desai Devika <131650004+Devi2806@users.noreply.github.com>
2026-05-10 17:36:31 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api'
+  baseURL: 'https://team-task-manager-production-58dc.up.railway.app/api'
 })
 
 API.interceptors.request.use((config) => {
